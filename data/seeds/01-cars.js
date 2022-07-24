2979ca4b7b3b450481924315beebbe922c8f3d7b
whitmajor <98010797+whitmajor@users.noreply.github.com>
2022-07-24 15:58:37 -0400

    added server and migration

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    { 
+        vin: "1111111111111",
+        make:"toyota",
+        model: "prius",
+        mileage: 215000,
+        title:"clean",
+        transmission: "manual",
+    },
+    { 
+        vin: "222222222222",
+        make:"toyota",
+        model: "corolla",
+        mileage: 115000,
+        title:"salvage",
+       
+    },
+    { 
+        vin: "33333333333",
+        make:"ford",
+        model: "focus",
+        mileage: 15000,
+      
+        
+    },
+]
+exports.seed = async function(knex){
+await knex("cars").truncate()
+await knex("cars").insert(cars)
+}
