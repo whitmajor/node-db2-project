09e82256e64cb593cb4c96180d25c89d7bece02b
whitmajor <98010797+whitmajor@users.noreply.github.com>
2022-07-24 16:55:25 -0400

    added codegrade

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,22 @@
+const db = require("../../data/db-config")
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('cars')
 }
 
 const getById = () => {
   // DO YOUR MAGIC
+  return db("cars").where("id", id).first()
 }
 
 const create = () => {
   // DO YOUR MAGIC
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create
+
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,28 @@
 // DO YOUR MAGIC
+const express = require("express")
+const Car = require("./cars-model")
+
+const router = express.Router()
+
+router.get("/", async(req,res,next)=>{
+    try{
+        const cars = await Car.getAll()
+        res.json(cars)
+    }catch(err){
+      next(err)
+    }
+})
+
+router.get("/:id", async(req,res,next)=>{
+    try{
+        const cars = await Car.getById(req.params.id)
+        res.json(cars)
+    }catch(err){
+      next(err)
+    }
+})
+
+router.post("/", async(req,res,next)=>{
+    res.json("posting new car")
+})
+module.exports = router
